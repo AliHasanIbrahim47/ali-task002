56249ffd5092694553e509be8f718804ec89f335
AliHasanIbrahim47 <ew9361903@gmail.com>
2024-03-09 13:57:14 +0300

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Web3 from 'web3';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('renders App component with block number and USDT balance', () => {
+  it('renders', () => {
+    render(<App />);
+
+    const learnReactLink = screen.getByText(/Ethereum Mainnet/i);
+    expect(learnReactLink).toBeInTheDocument();
+
+    const loadingMessage = screen.getByText(/Loading.../);
+    expect(loadingMessage).toBeInTheDocument();
+
+    const blockNumber = screen.getByText(/Last block number: \d+/);
+    expect(blockNumber).toBeInTheDocument();
+
+    const usdtBalance = screen.getByText(/USDT balance: \d+ USDT/);
+    expect(usdtBalance).toBeInTheDocument();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import Web3 from 'web3';
 import './App.css';
 
 function App() {
+  const [latestBlockNumber, setLatestBlockNumber] = useState<number | null>(null);
+  const [usdtBalance, setUsdtBalance] = useState<number | null>(null);
+  const usdtContractAddress: string = '0xdac17f958d2ee523a2206206994597c13d831ec7';
+  const ali_infura_url: string = 'https://mainnet.infura.io/v3/64be81cefae846b6909e5557cccc514c';
+
+  const web3 = new Web3(ali_infura_url);
+
+  async function fetchLatestBlockNumber(): Promise<void> {
+    try {
+      const blockNumber: bigint = await web3.eth.getBlockNumber();
+      const blockNumberAsNumber = Number(blockNumber);
+      setLatestBlockNumber(blockNumberAsNumber);
+
+    } catch (error) {
+      console.error('Error fetching block number: ', error);
+    }
+  }
+
+  async function fetchUsdtBalance(): Promise<void> {
+    try {
+      const balance: bigint = await web3.eth.getBalance(usdtContractAddress);
+      const balanceAsString = Number(balance);
+      setUsdtBalance(balanceAsString);
+    } catch (error) {
+      console.error('Error fetching USDT balance: ', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchLatestBlockNumber();
+    fetchUsdtBalance();
+  }, [latestBlockNumber, usdtBalance]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ethereum Mainnet</h1>
+      {latestBlockNumber !== null ? (
+        <>
+          <p>Last block number: {latestBlockNumber}</p>
+          {usdtBalance !== null && <p>USDT balance: {usdtBalance} USDT</p>}
+        </>
+      ) : (
+        <div className="loading-message">
+          <p>Loading...</p>
+          <div className="loading-spinner"></div>
+        </div>
+      )}
     </div>
   );
 }
